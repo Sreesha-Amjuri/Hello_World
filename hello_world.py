5a94c9f2b1ef9ac33707651c6fbce481d52c7555
Sreesha-Amjuri <sreeshaamjuri@gmail.com>
2019-03-08 00:06:49 +0530

    Code added commit 2

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,12 @@
-#! /bin/python
-
+#!/usr/bin/python
 import random
 
-print "Random Number : " random.random()
+# First random number
+print "random() : ", random.random()
+
+# Second random number
+print "random() : ", random.random()
+
 print {"Hello World"}
 print {"Hello World my first program"}
 print {"Hello World my first program 3rd line"}
