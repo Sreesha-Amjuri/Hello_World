84ed0e3bf0bf2b8b26430e43d5da39dd20c0fde4
Sreesha-Amjuri <sreeshaamjuri@gmail.com>
2019-03-08 00:03:11 +0530

    Added random function.

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,8 @@
+#! /bin/python
+
+import random
+
+print "Random Number : " random.random()
 print {"Hello World"}
 print {"Hello World my first program"}
 print {"Hello World my first program 3rd line"}
